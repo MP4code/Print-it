227e3deaea97d51bd5be3e1d24c66369768369ec
Marina <marina.philogene@gmail.com>
2024-04-23 13:48:14 +0200

    creation dots

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+const slides = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span> en boutique et en ligne </span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+let index = 0;
+const dots = document.querySelector(".dots");
+
+const generateDots = () => {
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("div");
+    if (i === index) {
+      dot.className = "dot dot_selected";
+    } else {
+      dot.className = "dot";
+    }
+    dots.appendChild(dot);
+  }
+};
+
+generateDots();
+
+/*
+for (let i = 0; i < slides.length; i++){
+
+
+
+
+
+const arrow_left  = document.querySelector(".arrow_left");
+arrow_left.addEventListener(
+  "click",
+  function () {
+	let dot = document.querySelector(".dot");
+	dot.classList.remove("dot"); 
+  },
+)
+console.log(arrow_left);
+
+
+
+*/
